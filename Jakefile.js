b98bf5f4c4052d6ddedb2a78d10f0dc0ff6ab921
Manuel Micu <manu4216@gmail.com>
2016-11-25 17:02:40 +0100

    add node version check to jake

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,33 @@
 (function() {
   'use strict';
 
+
+
   desc('Default build');
-  task('default', function() {
+  task('default', ['version'], function() {
     console.log("\n\nBUILD OK");
   });
+
+  desc('Checking node version');
+  task('version', function() {
+    var packageJson = require('./package.json');
+
+    console.log('Checking node version...');
+    var expectedVersion = "v" + packageJson.engines.node;
+    var actualNodeVersion = process.version;
+
+    if (actualNodeVersion !== expectedVersion) {
+      var stringLog = "node version doesn't match."
+                    + " Expected: " + expectedVersion
+                    + ". Actual: "  + actualNodeVersion;
+      fail(stringLog);
+    } else {
+      // All good. No need to say anything.
+    }
+
+  });
+
+  //var actualNodeVersion = process.version;
+
 
 }());
