fd97b53344a5a15951b2f651944e63a133c9a7be
Manuel Micu <manu4216@gmail.com>
2016-11-27 20:34:43 +0100

    added jake task for running a server locally

--- Jakefile.js
+++ Jakefile.js
@@ -1,14 +1,26 @@
-/* globals desc: flase, task: false, complete: false, fail: false */
+/* globals jake:false, desc:false, task:false, complete:false, fail:false */
 
 (function() {
   'use strict';
 
   var jshint = require('simplebuild-jshint');
 
+  //*** General purpose tasks
+
   desc('Default build');
   task('default', ['version', 'lint'], function() {
     console.log("\n\nBUILD OK");
   });
+
+
+  desc('Run a localhost server');
+  task('run', function() {
+    jake.exec('node node_modules/http-server/bin/http-server src', { interactive: true }, complete);
+    console.log('Running http-server');
+  }, { async: true });
+
+
+  //*** Supporting tasks
 
   desc('Checking node version');
   task('version', function() {
--- readme.md
+++ readme.md
@@ -3,4 +3,7 @@ How to - tabs
 
 To run the build:
 1. Install jake: `npm install jake -g`
-2. Run jake: unix:`./jake`, windows: `jake` 
+2. Run jake build: unix:`./jake`, windows: `jake`
+
+To run a local server:
+1. Run jake run: `jake run`
