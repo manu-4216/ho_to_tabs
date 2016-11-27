aece3ae43c5a7a37e5ed3f3befcbd27ed9b01596
Manuel Micu <manu4216@gmail.com>
2016-11-27 15:24:16 +0100

    automated jshint into jake

--- Jakefile.js
+++ Jakefile.js
@@ -4,7 +4,7 @@
 
 
   desc('Default build');
-  task('default', ['version'], function() {
+  task('default', ['version', 'lint'], function() {
     console.log("\n\nBUILD OK");
   });
 
@@ -17,9 +17,9 @@
     var actualNodeVersion = process.version;
 
     if (actualNodeVersion !== expectedVersion) {
-      var stringLog = "node version doesn't match."
-                    + " Expected: " + expectedVersion
-                    + ". Actual: "  + actualNodeVersion;
+      var stringLog = "node version doesn't match." +
+                      " Expected: " + expectedVersion +
+                      ". Actual: "  + actualNodeVersion;
       fail(stringLog);
     } else {
       // All good. No need to say anything.
@@ -27,7 +27,11 @@
 
   });
 
-  //var actualNodeVersion = process.version;
+  desc('Lint check');
+  task('lint', function(){
+    console.log('Linting JS...');
 
+    jake.exec('node node_modules/jshint/bin/jshint Jakefile.js', { interactive: true }, complete);
+  }, { async: true });
 
 }());
